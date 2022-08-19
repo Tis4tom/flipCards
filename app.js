084df262b6fe88f1ee5a13d1b2a8bf125cd0bdf2
Tis4tom <tdmorgan_mac@icloud.com>
2022-08-19 18:45:12 -0400

    add initial design and functionality

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const cards = document.querySelectorAll('.card');
+// select all cards on the page and with for of loop listen for click
 for (let card of cards) {
 card.addEventListener( 'click', function() {
+ //for each card not 'this' clicked, set back to front facing
   cards.forEach(c => {
-    if(c !== this) {
-    c.classList.remove('is-flipped');
-    c.classList.add('delayed');
-    }
+    if (c !== this) {
+      c.classList.remove('is-flipped');
+      c.classList.add('delayed');
+    } 
   })
   this.classList.toggle('is-flipped');
   this.classList.remove('delayed');
